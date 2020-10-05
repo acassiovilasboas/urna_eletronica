0fee428dc08920c76cf7a408f79caec83dfec309
Acassio Vilas Boas <acassio.dev@outlook.com>
2020-10-05 08:22:39 -0300

    enviando projeto para github

---
+++ etapas.js
@@ -0,0 +1,62 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '11111',
+                nome: 'Anja Clara',
+                partido: 'VERDE',
+                fotos: [
+                    {
+                        url: 'anja_clara.jpg', legenda: 'Vereador', small: false 
+                    }
+                ]
+            },
+            {
+                numero: '22222',
+                nome: 'José Wynder',
+                partido: 'FORTNITE',
+                fotos: [
+                    {
+                        url: 'jose_wynder.jpg', legenda: 'Vereador', small: false
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Acássio Mendonça',
+                partido: 'SSS',
+                vice: 'Maiane Reis',
+                fotos: [
+                    {
+                        url:'acassio.jpg', legenda: 'Prefeito'
+                    },
+                    {
+                        url:'maiane.jpg', legenda: 'Vice-Prefeito', small: true
+                    }
+                ]
+            },
+            {
+                numero: '22',
+                nome: 'Maiane Reis',
+                partido: 'QQQ',
+                vice: 'Acássio Mendonça',
+                fotos: [
+                    {
+                        url: 'maiane.jpg', legenda: 'Prefeito'
+                    },
+                    {
+                        url: 'acassio.jpg', legenda: 'Vice-Prefeito', small: true
+                    }
+                ]
+            }
+        ]
+    }
+]
